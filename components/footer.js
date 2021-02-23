dd1f4620f799613c58f71c5cc075851a60e2366c
ptums <ptumulty923@gmail.com>
2021-02-23 17:00:18 -0500

    started building out a footer

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,113 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Link from 'next/link';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import styles from 'styles/Footer.module.css';
+
 export default function Footer() {
-  return <footer>Footer will go here :)</footer>;
+  return (
+    <footer className={styles.footerContainer}>
+      <Container>
+        <Row>
+          <Col sm={12} md={8}>
+            <div className="d-flex py-4">
+              <div className="me-5">
+                <h4 className={styles.title}>Navigation</h4>
+                <ul className={`${styles.footerList} list-unstyled`}>
+                  <li>
+                    <Link href="/services">
+                      <a>
+                        Services
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/gallery">
+                      <a>
+                        Gallery
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/careers">
+                      <a>
+                        Careers
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/contact">
+                      <a>
+                        Contact
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/sitemap.xml">
+                      <a>
+                        Sitemaps
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/privacy-policy">
+                      <a>
+                        Privacy Policy
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="mx-5">
+                <h4 className={styles.title}>Social Media</h4>
+                <ul className={`${styles.footerList} list-unstyled`}>
+                  <li>
+                    <Link href="/services">
+                      <a>
+                        Facebook
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/gallery">
+                      <a>
+                        Instagram
+                      </a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/careers">
+                      <a>
+                        Tik Tok
+                      </a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="mx-5">
+                <h4 className={styles.title}>Contact</h4>
+                <ul className={`${styles.footerList} list-unstyled`}>
+                  <li>
+                    <p className="mb-0 mt-3">
+                      <strong className={styles.blueTitle}>Email: </strong>
+                      wavelandscapingdesign@gmail.com
+                    </p>
+                    <p>
+                      <strong className={styles.blueTitle}>Phone: </strong>
+                      516-319-3145
+                    </p>
+                    <p><strong className={styles.blueTitle}>Based out of Palm Beach, Florida</strong></p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Col>
+          <Col sm={12} md={4}>
+            Map
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
