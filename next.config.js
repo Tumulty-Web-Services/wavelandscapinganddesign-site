e7033118b33836b6b23d368d781de5cac6837e15
ptums <ptumulty923@gmail.com>
2021-02-25 08:50:18 -0500

    added sitemap generator

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
     domains: ['res.cloudinary.com'],
   },
   target: 'serverless',
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./utils/generate-sitemap');
+    }
+
+    return config;
+  },
 };
