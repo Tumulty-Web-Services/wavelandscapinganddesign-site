66b92e5cadb054625e208f362c12be4de0f26588
ptums <ptumulty923@gmail.com>
2021-03-04 15:23:21 -0500

    built out success page

--- pages/success.js
+++ pages/success.js
@@ -1,5 +1,28 @@
+import Link from 'next/link';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export default function Success() {
   return (
-    <div>Great! The form was submitted :) </div>
-  )
+    <Container className="py-5 my-5">
+      <Row>
+        <Col sm={12} md={{span: 6, offset:3}}>
+          <h2 className="display-4">Thank you for reaching out!</h2>
+          <h4 className="text-center">
+            <span className="lead">
+              We'll be reaching out to you shortly about your inquiry.
+            </span>
+            {' '}
+            <strong>Have a great day!</strong>
+          </h4>
+          <Link href="/">
+            <a className="d-block my-3 text-center">
+              <u>Return to the homepage</u>
+            </a>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
